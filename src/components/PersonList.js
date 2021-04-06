e4f47d91efccbf237dfbd1cd905e9161c8e1c4c5
Pablocc0 <pabloandroidcc0@gmail.com>
2021-04-06 02:35:25 -0300

    Primeiro push

---
+++ src/components/PersonList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import axios from 'axios';
+
+import {
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        console.log(persons);
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+    )
+  }
+}
---
+++ src/views/Index.js
@@ -0,0 +1,329 @@
+import React, { useState, useEffect } from "react";
+
+import axios from 'axios';
+// node.js library that concatenates classes (strings)
+import classnames from "classnames";
+// reactstrap components
+import {
+  Badge,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Modal,
+  Col,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  CardFooter,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Media,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+import Header from "components/Headers/Header.js";
+
+const Index = (props) => {
+  const [activeNav, setActiveNav] = useState(1);
+  const [chartExample1Data, setChartExample1Data] = useState("data1");
+
+  const toggleNavs = (e, index) => {
+    e.preventDefault();
+    setActiveNav(index);
+    setChartExample1Data("data" + index);
+  };
+
+
+  return (
+    <>
+      <Header />
+      {/* Page content */}
+      <Container className="mt--7" fluid>
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <h3 className="mb-0">Produtos para entrega</h3>
+
+              </CardHeader>
+              <Table className="align-items-center table-flush" responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Nome do Cliente</th>
+                    <th scope="col">Descrição do Produto</th>
+                    <th scope="col">Status da venda</th>
+                    <th scope="col">Status da entrega</th>
+                    <th scope="col" />
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">
+                      <Media className="align-items-center">
+                        <a
+                          className="avatar rounded-circle mr-3"
+                          href="#pablo"
+                          onClick={(e) => e.preventDefault()}
+                        >
+                          <img
+                            alt="..."
+                            src={
+                              require("../assets/img/theme/bootstrap.jpg")
+                                .default
+                            }
+                          />
+                        </a>
+                        <Media>
+                          <span className="mb-0 text-sm">
+                            Josenilson
+                          </span>
+                        </Media>
+                      </Media>
+                    </th>
+                    <td>Chuveiro Eletrico</td>
+                    <td>
+                      <Badge color="success" className="badge-dot mr-4">
+                        <i className="bg-success" />
+                        Finalizado
+                      </Badge>
+                    </td>
+                    <td>
+                      <Badge color="success" href="#pablo"
+                        onClick={e => e.preventDefault(alert('Produto entregue com sucesso!'))} >
+                        <i className="bg-success" />
+                        Entregue
+                      </Badge>
+                    </td>
+                    <td className="text-right">
+                      <UncontrolledDropdown>
+                        <DropdownToggle
+                          className="btn-icon-only text-light"
+                          href="#pablo"
+                          role="button"
+                          size="sm"
+                          color=""
+                          onClick={(e) => e.preventDefault()}
+                        >
+                          <i className="fas fa-ellipsis-v" />
+                        </DropdownToggle>
+                        <DropdownMenu className="dropdown-menu-arrow" right>
+                          <DropdownItem
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            Confirmar Entregua
+                          </DropdownItem>
+                          <DropdownItem
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            Cancelar
+                          </DropdownItem>
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <Media className="align-items-center">
+                        <a
+                          className="avatar rounded-circle mr-3"
+                          href="#pablo"
+                          onClick={(e) => e.preventDefault()}
+                        >
+                          <img
+                            alt="..."
+                            src={
+                              require("../assets/img/theme/bootstrap.jpg")
+                                .default
+                            }
+                          />
+                        </a>
+                        <Media>
+                          <span className="mb-0 text-sm">
+                            Abnoan
+                          </span>
+                        </Media>
+                      </Media>
+                    </th>
+                    <td>Tinta Suvinil</td>
+                    <td>
+                      <Badge color="" className="badge-dot mr-4">
+                        <i className="bg-success" />
+                        Finalizado
+                      </Badge>
+                    </td>
+                    <td>
+                      <Badge color="warning" >
+                        <i className="bg-warning" />
+                        Pendente
+                      </Badge>
+                    </td>
+                    <td className="text-right">
+                      <UncontrolledDropdown>
+                        <DropdownToggle
+                          className="btn-icon-only text-light"
+                          href="#pablo"
+                          role="button"
+                          size="sm"
+                          color=""
+                          onClick={(e) => e.preventDefault()}
+                        >
+                          <i className="fas fa-ellipsis-v" />
+                        </DropdownToggle>
+                        <DropdownMenu className="dropdown-menu-arrow" right>
+                          <DropdownItem
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            Confirmar Entregua
+                          </DropdownItem>
+                          <DropdownItem
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            Cancelar
+                          </DropdownItem>
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <Media className="align-items-center">
+                        <a
+                          className="avatar rounded-circle mr-3"
+                          href="#pablo"
+                          onClick={(e) => e.preventDefault()}
+                        >
+                          <img
+                            alt="..."
+                            src={
+                              require("../assets/img/theme/bootstrap.jpg")
+                                .default
+                            }
+                          />
+                        </a>
+                        <Media>
+                          <span className="mb-0 text-sm">
+                            Marley
+                          </span>
+                        </Media>
+                      </Media>
+                    </th>
+                    <td>Furadeira Bosch</td>
+                    <td>
+                      <Badge color="" className="badge-dot mr-4">
+                        <i className="bg-danger" />
+                        Aguardando
+                      </Badge>
+                    </td>
+                    <td>
+                      <Badge color="warning" >
+                        <i className="bg-warning" />
+                        Pendente
+                      </Badge>
+                    </td>
+                    <td className="text-right">
+                      <UncontrolledDropdown>
+                        <DropdownToggle
+                          className="btn-icon-only text-light"
+                          href="#pablo"
+                          role="button"
+                          size="sm"
+                          color=""
+                          onClick={(e) => e.preventDefault()}
+                        >
+                          <i className="fas fa-ellipsis-v" />
+                        </DropdownToggle>
+                        <DropdownMenu className="dropdown-menu-arrow" right>
+                          <DropdownItem
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            Confirmar Entregua
+                          </DropdownItem>
+                          <DropdownItem
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            Cancelar
+                          </DropdownItem>
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+              <CardFooter className="py-4">
+                <nav aria-label="...">
+                  <Pagination
+                    className="pagination justify-content-end mb-0"
+                    listClassName="justify-content-end mb-0"
+                  >
+                    <PaginationItem className="disabled">
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                        tabIndex="-1"
+                      >
+                        <i className="fas fa-angle-left" />
+                        <span className="sr-only">Previous</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem className="active">
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        1
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        2 <span className="sr-only">(current)</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        3
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className="fas fa-angle-right" />
+                        <span className="sr-only">Next</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                  </Pagination>
+                </nav>
+              </CardFooter>
+            </Card>
+          </div>
+        </Row>
+
+      </Container>
+    </>
+  );
+};
+
+export default Index;
